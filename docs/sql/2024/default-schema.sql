3d8c64bbc0faf0ea3d579489c9b600550e517441
Naufal Haris <naufal.michiels@gmail.com>
2025-07-13 15:55:11 +0700

    refactor(user): correct user logic to support postgres

--- docs/sql/2024/default-schema.sql
+++ docs/sql/2024/default-schema.sql
@@ -1,43 +1,39 @@
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `fk_role_id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `refresh_token` VARCHAR(255),
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users
+(
+    "id"            SERIAL PRIMARY KEY,
+    "fk_role_id"    INT            NOT NULL,
+    "name"          VARCHAR(255)   NOT NULL,
+    "email"         VARCHAR(255)   NOT NULL,
+    "password"      VARCHAR(255)   NOT NULL,
+    "refresh_token" VARCHAR(255),
 
     -- Utility columns
-    `status` SMALLINT NOT NULL DEFAULT '1',
-    `flag` INT NOT NULL DEFAULT '0',
-    `meta` VARCHAR(255),
-    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `created_by` VARCHAR(255),
-    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `updated_by` VARCHAR(255),
-    `deleted_at`TIMESTAMP,
-    `deleted_by` VARCHAR(255),
-    PRIMARY KEY (`id`)
-) ENGINE = INNODB;
+    "status"        SMALLINT       NOT NULL DEFAULT 1,
+    "flag"          INT            NOT NULL DEFAULT 0,
+    "meta"          VARCHAR(255),
+    "created_at"    TIMESTAMPTZ    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by"    VARCHAR(255),
+    "updated_at"    TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_by"    VARCHAR(255),
+    "deleted_at"    TIMESTAMPTZ,
+    "deleted_by"    VARCHAR(255)
+);
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `role` VARCHAR(255) NOT NULL,
+DROP TABLE IF EXISTS roles;
+CREATE TABLE IF NOT EXISTS roles
+(
+    "id"         SERIAL PRIMARY KEY,
+    "role"       VARCHAR(255) NOT NULL,
 
     -- Utility columns
-    `status` SMALLINT NOT NULL DEFAULT '1',
-    `flag` INT NOT NULL DEFAULT '0',
-    `meta` VARCHAR(255),
-    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `created_by` VARCHAR(255),
-    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `updated_by` VARCHAR(255),
-    `deleted_at`TIMESTAMP,
-    `deleted_by` VARCHAR(255),
-    PRIMARY KEY (`id`)
-) ENGINE = INNODB;
-
-INSERT INTO role (role)
-VALUES ('admin'),
-       ('user');
+    "status"     SMALLINT     NOT NULL DEFAULT 1,
+    "flag"       INT          NOT NULL DEFAULT 0,
+    "meta"       VARCHAR(255),
+    "created_at" TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" VARCHAR(255),
+    "updated_at" TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_by" VARCHAR(255),
+    "deleted_at" TIMESTAMPTZ,
+    "deleted_by" VARCHAR(255)
+);
