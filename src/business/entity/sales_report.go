45739d069b41b6ceea1b43ebb92397ea1467b4cb
Naufal Haris <naufal.michiels@gmail.com>
2025-07-13 20:29:26 +0700

    refactor(db): remove status column from sales report table

--- src/business/entity/sales_report.go
+++ src/business/entity/sales_report.go
@@ -4,18 +4,10 @@ import "time"
 
 type Status string
 
-const (
-	Waiting    Status = "WAITING"
-	Processing Status = "PROCESSING"
-	Done       Status = "DONE"
-	Failed     Status = "FAILED"
-)
-
 type SalesReport struct {
 	Id           int64     `db:"id"`
 	UserId       int64     `db:"user_id"`
 	FileUrl      string    `db:"file_url"`
-	Status       Status    `db:"status"`
 	ErrorMessage string    `db:"error_message"`
 	StartAt      time.Time `db:"start_at"`
 	CompletedAt  time.Time `db:"completed_at"`
