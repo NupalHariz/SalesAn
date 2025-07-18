90a5d347895930e8ff1de091b8e499353cf0764d
Naufal Haris <naufal.michiels@gmail.com>
2025-07-19 01:31:30 +0700

    feat(product-summaries): add domain for product summaries

---
+++ src/business/domain/product_summary/product_summary.go
@@ -0,0 +1,39 @@
+package productsummary
+
+import (
+	"context"
+
+	"github.com/NupalHariz/SalesAn/src/business/entity"
+	"github.com/reyhanmichiels/go-pkg/v2/log"
+	"github.com/reyhanmichiels/go-pkg/v2/sql"
+)
+
+type Interface interface {
+	Create(ctx context.Context, param []entity.ProductSummary) error
+}
+
+type productSummary struct {
+	db  sql.Interface
+	log log.Interface
+}
+
+type InitParam struct {
+	Db  sql.Interface
+	Log log.Interface
+}
+
+func Init(param InitParam) Interface {
+	return &productSummary{
+		db:  param.Db,
+		log: param.Log,
+	}
+}
+
+func (p *productSummary) Create(ctx context.Context, param []entity.ProductSummary) error {
+	err := p.createSQL(ctx, param)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ src/business/domain/product_summary/product_summary_sql.go
@@ -0,0 +1,41 @@
+package productsummary
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/NupalHariz/SalesAn/src/business/entity"
+	"github.com/reyhanmichiels/go-pkg/v2/codes"
+	"github.com/reyhanmichiels/go-pkg/v2/errors"
+	"github.com/reyhanmichiels/go-pkg/v2/sql"
+)
+
+func (p *productSummary) createSQL(ctx context.Context, param []entity.ProductSummary) error {
+	p.log.Info(ctx, fmt.Sprintf("insert to sales_sumarries with param: %v", param))
+	
+	tx, err := p.db.Leader().BeginTx(ctx, "txProductSummary", sql.TxOptions{})
+	if err != nil {
+		return errors.NewWithCode(codes.CodeSQLTxBegin, err.Error())
+	}
+	defer tx.Rollback()
+
+	res, err := tx.NamedExec("iNewProductSummary", insertProductSummary, param)
+	if err != nil {
+		return errors.NewWithCode(codes.CodeSQLTxExec, err.Error())
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		return errors.NewWithCode(codes.CodeSQLNoRowsAffected, err.Error())
+	} else if rowCount < 1 {
+		return errors.NewWithCode(codes.CodeSQLNoRowsAffected, "no product summary created")
+	}
+
+	if err = tx.Commit(); err != nil {
+		return errors.NewWithCode(codes.CodeSQLTxCommit, err.Error())
+	}
+
+	p.log.Info(ctx, fmt.Sprintf("success to create product summary with param: %v", param))
+
+	return nil
+}
